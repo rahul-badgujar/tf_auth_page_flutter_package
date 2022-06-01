39b6b01a0082723d8f4c294a8c02f12b4043e935
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-06-02 01:36:20 +0530

    methods extracted

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,32 +23,38 @@ class MyApp extends StatelessWidget {
         ],
         authProvider:
             TfAuthFirebase(firebaseAuthInstance: FirebaseAuth.instance),
-        onAuthOperationSuccess: (context, operation, user) async {
-          if (operation is TfLoginOperation) {
-            showMessagedSnackbar(
-                context, 'Logged in successfully (uid:${user.email})');
-          } else if (operation is TfSignupOperation) {
-            showMessagedSnackbar(
-                context, 'Registered successfully (uid:${user.email})');
-          } else if (operation is TfForgotPasswordOperation) {
-            showMessagedSnackbar(
-                context, 'Resetted password successfully (uid:${user.email})');
-          }
-        },
-        onAuthOperationFailed: (context, operation, error) async {
-          if (operation is TfLoginOperation) {
-            showMessagedSnackbar(context, 'Failed to login: $error');
-          } else if (operation is TfSignupOperation) {
-            showMessagedSnackbar(context, 'Failed to register: $error');
-          } else if (operation is TfForgotPasswordOperation) {
-            showMessagedSnackbar(context, 'Failed to reset password: $error');
-          }
-        },
-        onCancel: (context) async {
-          showMessagedSnackbar(context, 'Authentication cancelled.');
-        },
+        onAuthOperationSuccess: _onAuthOperationSuccess,
+        onAuthOperationFailed: _onAuthOperationFailed,
+        onCancel: _onCancel,
       ),
     );
+  }
+
+  Future<void> _onCancel(context) async {
+    showMessagedSnackbar(context, 'Authentication cancelled.');
+  }
+
+  Future<void> _onAuthOperationFailed(context, operation, error) async {
+    if (operation is TfLoginOperation) {
+      showMessagedSnackbar(context, 'Failed to login: $error');
+    } else if (operation is TfSignupOperation) {
+      showMessagedSnackbar(context, 'Failed to register: $error');
+    } else if (operation is TfForgotPasswordOperation) {
+      showMessagedSnackbar(context, 'Failed to reset password: $error');
+    }
+  }
+
+  Future<void> _onAuthOperationSuccess(context, operation, user) async {
+    if (operation is TfLoginOperation) {
+      showMessagedSnackbar(
+          context, 'Logged in successfully (uid:${user.email})');
+    } else if (operation is TfSignupOperation) {
+      showMessagedSnackbar(
+          context, 'Registered successfully (uid:${user.email})');
+    } else if (operation is TfForgotPasswordOperation) {
+      showMessagedSnackbar(
+          context, 'Resetted password successfully (uid:${user.email})');
+    }
   }
 
   void showMessagedSnackbar(BuildContext context, String message) {
